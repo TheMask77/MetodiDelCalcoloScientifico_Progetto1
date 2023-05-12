30bd4548f96caed5a723a93da5f72d934a3a774b
TheMask77 <m.carissimi4@campus.unimib.it>
2023-05-12 16:02:35 +0200

    Created JacobiSolver class and implemented first utility methods

---
+++ RisolutoreSistemiLineari/src/main/java/org/Risolutore/JacobiSolver.java
@@ -0,0 +1,42 @@
+package org.Risolutore;
+
+import org.ejml.simple.SimpleMatrix;
+
+public class JacobiSolver {
+
+    public static SimpleMatrix solve(SimpleMatrix inputMatrix, SimpleMatrix rightHandSide, int maxIterations, double tolerance) {
+        SimpleMatrix currentSolution = new SimpleMatrix(inputMatrix.getNumRows(), 1);
+
+
+
+        return currentSolution;
+    }
+
+    public static SimpleMatrix getPMatrix(SimpleMatrix inputMatrix) {
+        SimpleMatrix pMatrix = new SimpleMatrix(inputMatrix.getNumRows(), inputMatrix.getNumCols());
+
+        for (int i = 0; i < pMatrix.getNumRows(); i++)
+            pMatrix.set(i, i, inputMatrix.get(i, i));
+
+        return pMatrix;
+    }
+
+    public static SimpleMatrix getInvertedPMatrix(SimpleMatrix inputMatrix) {
+        SimpleMatrix invertedPMatrix = new SimpleMatrix(inputMatrix.getNumRows(), inputMatrix.getNumCols());
+
+        for (int i = 0; i < invertedPMatrix.getNumRows(); i++)
+            invertedPMatrix.set(i, i, 1/inputMatrix.get(i, i));
+
+        return invertedPMatrix;
+    }
+
+    public static SimpleMatrix getNMatrix(SimpleMatrix inputMatrix) {
+        SimpleMatrix nMatrix = inputMatrix.copy();
+
+        for (int i = 0; i < nMatrix.getNumRows(); i++)
+            nMatrix.set(i, i, 0);
+
+        return nMatrix;
+    }
+
+}
--- RisolutoreSistemiLineari/src/main/java/org/Risolutore/Main.java
+++ RisolutoreSistemiLineari/src/main/java/org/Risolutore/Main.java
@@ -8,60 +8,43 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         double tol;
 
-        String inputFilePath ="D:/Download/Dati Progetto 1 Bis-20230418/vem2.mtx";
-        SimpleMatrix inputMatrixTest = Utils.loadSimpleMatrixFromFile(inputFilePath);
-        new SimpleMatrix(inputMatrixTest.getNumRows(), 1);
-        SimpleMatrix solverSolutionTest;
-        SimpleMatrix exactSolutionTest = new SimpleMatrix(inputMatrixTest.getNumRows(), 1);
-        SimpleMatrix startingSolutionTest = new SimpleMatrix(inputMatrixTest.getNumRows(), 1);
+        //Initializing
+        String inputFilePath ="D:/Download/Dati Progetto 1 Bis-20230418/testing.mtx";
+        SimpleMatrix inputMatrix = Utils.loadSimpleMatrixFromFile(inputFilePath);
+        new SimpleMatrix(inputMatrix.getNumRows(), 1);
+        SimpleMatrix solverSolution;
+        SimpleMatrix exactSolution = new SimpleMatrix(inputMatrix.getNumRows(), 1);
+        SimpleMatrix startingSolution = new SimpleMatrix(inputMatrix.getNumRows(), 1);
 
-        for (int i = 0; i < exactSolutionTest.getNumRows(); i++) {
-            exactSolutionTest.set(i, 0, 1);
-            startingSolutionTest.set(i, 0, 0);
-        }
-
-        SimpleMatrix rightHandSideTest = inputMatrixTest.mult(exactSolutionTest);
-
-        long startTime = System.nanoTime();
-        System.out.println("Starting now");
-
-        solverSolutionTest = GaussSeidelSolver.solve(inputMatrixTest, rightHandSideTest, 20000, 0.00001);
-
-        long endTime = System.nanoTime();
-
-        System.out.println("Solutions found in " + (endTime - startTime)/1000000000 + " seconds");
-        System.out.println("Solver");
-        solverSolutionTest.transpose().print();
-        System.out.println("Exact");
-        exactSolutionTest.transpose().print();
-
-
-
-//====================================================================================================
-
-/*
-        DMatrixSparseCSC inputMatrix = Utils.loadMatrixFromFile(inputFilePath);
-
-        DMatrixSparseCSC exactSolution = new DMatrixSparseCSC(inputMatrix.getNumRows(), 1);
-        DMatrixSparseCSC startingSolution = new DMatrixSparseCSC(inputMatrix.getNumRows(), 1);
         for (int i = 0; i < exactSolution.getNumRows(); i++) {
             exactSolution.set(i, 0, 1);
             startingSolution.set(i, 0, 0);
         }
 
-        DMatrixSparseCSC rightHandSide = new DMatrixSparseCSC(inputMatrix.getNumRows(), 1);
-        CommonOps_DSCC.mult(inputMatrix, exactSolution, rightHandSide);
+        SimpleMatrix rightHandSide = inputMatrix.mult(exactSolution);
 
-        Solvers solvers = new Solvers(inputMatrix, startingSolution, rightHandSide, 0.0);
-        DMatrixSparseCSC solverSolution = solvers.gaussSeidel();
+        //Other testing
+        inputMatrix.print();
+        JacobiSolver.getPMatrix(inputMatrix).print();
+        JacobiSolver.getInvertedPMatrix(inputMatrix).print();
+        JacobiSolver.getNMatrix(inputMatrix).print();
 
-        System.out.println("Solutions with my solver: ");
+
+        //Testing the solver
+        /*
+        long startTime = System.nanoTime();
+        System.out.println("Starting now");
+
+        solverSolution = GaussSeidelSolver.solve(inputMatrix, rightHandSide, 20000, 0.0000000001);
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Solutions found in " + (endTime - startTime)/1000000000 + " seconds");
         System.out.println("Solver");
-        CommonOps_DSCC.transpose(solverSolution, null,null).print();
+        solverSolution.transpose().print();
         System.out.println("Exact");
-        CommonOps_DSCC.transpose(exactSolution, null,null).print();
-
- */
+        exactSolution.transpose().print();
+         */
 
     }
 }
