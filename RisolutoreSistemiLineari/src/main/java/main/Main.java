3be51e74caef12282b52d4158f0af46b940883da
Lorenzo Colombo <lorenzo.colombo@gnsspa.it>
2023-05-27 22:49:38 +0200

    added tolerance selection; added option to run selected method on all the matrix available

--- RisolutoreSistemiLineari/src/main/java/main/Main.java
+++ RisolutoreSistemiLineari/src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import org.ejml.equation.MatrixConstructor;
 import solvers.*;
 import org.ejml.simple.SimpleMatrix;
 
@@ -9,6 +10,7 @@ import java.util.Scanner;
 
 public class Main {
     private static int answer;
+    private static File[] listOfFiles;
     private static String question;
     private static final Scanner keyboard = new Scanner(System.in);
     private static final String defaultFilePath ="src/files/";
@@ -16,6 +18,33 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         //Variables Declaration
+        double tolerance = 0;
+        int fileNumber = 0;
+        Solver solver = null;
+        String filePath = "";
+
+        //User Interaction
+        solver = MethodSelection();
+        tolerance = ToleranceSelection();
+        filePath = FileSelection();
+
+        //Checks for option all files
+        try{
+            if(Integer.parseInt(filePath) == listOfFiles.length + 1)  fileNumber = listOfFiles.length;
+        }catch(NumberFormatException e){
+            MatrixEvaluation(solver, filePath, tolerance);
+        }
+        for(int i = 0; i < fileNumber; i++){
+            if (listOfFiles[i].isFile()) {
+                filePath = defaultFilePath + listOfFiles[i].getName();
+                MatrixEvaluation(solver, filePath, tolerance);
+            }
+        }
+    }
+
+    private static void MatrixEvaluation(Solver solver, String filePath, double tolerance) {
+
+        int maxIterations = 20000;
         long startTime;
         long endTime;
         SimpleMatrix inputMatrixTest;
@@ -23,27 +52,22 @@ public class Main {
         SimpleMatrix rightHandSide = null;
         SimpleMatrix startingSolution = null;
         SimpleMatrix solverSolution = null;
-        Solver solver = null;
-        String filePath = "";
 
-        //User Interaction
-        solver = MethodSelection();
-        filePath = FileSelection();
+        System.out.println("\n\n========================= PROCEDURE STARTUP FOR " + filePath.split("/")[2] + " =========================");
 
         //Matrix Setup
         inputMatrixTest = Utils.loadSimpleMatrixFromFile(filePath);
         exactSolution = new SimpleMatrix(inputMatrixTest.getNumRows(), 1);
         startingSolution = new SimpleMatrix(inputMatrixTest.getNumRows(), 1);
-        for (int i = 0; i < inputMatrixTest.getNumRows(); i++) {
-            exactSolution.set(i, 0, 1);
-            startingSolution.set(i, 0, 0);
+        for (int j = 0; j < inputMatrixTest.getNumRows(); j++) {
+            exactSolution.set(j, 0, 1);
+            startingSolution.set(j, 0, 0);
         }
         rightHandSide = inputMatrixTest.mult(exactSolution);
 
         //Solution Calculation
         startTime = System.nanoTime();
-        System.out.println("Starting now...");
-        solverSolution = solver.solve(inputMatrixTest, rightHandSide, 20000, 0.00001);
+        solverSolution = solver.solve(inputMatrixTest, rightHandSide, maxIterations, tolerance);
         endTime = System.nanoTime();
         System.out.println("Solutions found in " + (endTime - startTime)/1000000000 + " seconds");
         System.out.println("Relative error ==> " + Utils.evaluateRelativeError(exactSolution, solverSolution));
@@ -53,10 +77,26 @@ public class Main {
         exactSolution.transpose().print();
     }
 
+    private static double ToleranceSelection(){
+
+        double[] tolerances = {0.0001, 0.000001, 0.00000001, 0.0000000001};
+
+        question = "Selezionare la tolleranza desiderata: ";
+        for(int i = 0; i < tolerances.length; i++){
+            question += "\n" + (i + 1) + ") " + tolerances[i];
+        }
+        do{
+            System.out.println(question);
+            answer = keyboard.nextInt();
+        }while(answer < 1 || answer > tolerances.length);
+
+        return tolerances[answer - 1];
+    }
+
     private static String FileSelection() {
         //File Selection
         File folder = new File(defaultFilePath);
-        File[] listOfFiles = folder.listFiles();
+        listOfFiles = folder.listFiles();
         question = "Selezionare una delle seguenti matrici: ";
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile()) {
@@ -69,9 +109,12 @@ public class Main {
             answer = keyboard.nextInt();
         }while(answer < 1 || answer > listOfFiles.length + 1);
 
-        return defaultFilePath + listOfFiles[answer - 1].getName();
-
-        //TODO implementation of all files choice
+        if(answer == listOfFiles.length + 1){
+            return String.valueOf(listOfFiles.length + 1);
+        }
+        else{
+            return defaultFilePath + listOfFiles[answer - 1].getName();
+        }
     }
 
     public static Solver MethodSelection(){
